f534ea0efc601fae475d4285dcfbb54923474c13
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-05-13 09:20:28 -0500

    Added source code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 /*=============== SEARCH BAR JS ===============*/
+const toggleSearch = (search, button) =>{
+   const searchBar = document.getElementById(search),
+         searchButton = document.getElementById(button)
+
+   searchButton.addEventListener('click', () =>{
+       // We add the show-search class, so that the search bar expands
+       searchBar.classList.toggle('show-search')
+   })
+}
+toggleSearch('search-bar', 'search-button')
